ab74020af14705f039c4b68d72f6f469941532de
quannh287 <quannh2871@gmail.com>
2022-12-22 16:18:20 +0700

    Dropdown comp

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './styles'
+
+const Dropdown = () => {
+  return (
+    <View>
+      <Text>Dropdown</Text>
+    </View>
+  )
+}
+
+export default Dropdown
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+import Dropdown from './components/Dropdown';
+
+export { Dropdown };
